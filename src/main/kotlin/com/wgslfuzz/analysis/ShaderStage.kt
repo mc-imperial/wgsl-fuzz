c06580f8ec0c68d7d965ee664f35ab2481de8940
Alastair F. Donaldson <alastair.donaldson@imperial.ac.uk>
2025-05-27 09:43:56 +0100

    Shader stage analysis: comments and tests
    Commentary and testing for shader stage analysis.

--- src/main/kotlin/com/wgslfuzz/analysis/ShaderStage.kt
+++ src/main/kotlin/com/wgslfuzz/analysis/ShaderStage.kt
@@ -18,18 +18,38 @@ enum class ShaderStage {
     COMPUTE,
 }
 
+/**
+ * Provides a mapping from function name to the shader stages from which that function might be
+ * invoked. The keys to this map are functions declared in the translation unit (i.e., builtin
+ * functions are not keys).
+ *
+ * @param tu the translation unit to be analysed
+ * @param environment the resolved environment for [tu]
+ * @return a map from each function declared in [tu] to the set of [ShaderStage]s from which it
+ *     can be (directly or indirectly) invoked.
+ */
 fun runFunctionToShaderStageAnalysis(
     tu: TranslationUnit,
     environment: ResolvedEnvironment,
 ): Map<String, Set<ShaderStage>> {
+    // The map that will ultimately be returned.
     val result: MutableMap<String, MutableSet<ShaderStage>> = mutableMapOf()
+
+    // The analyses uses BFS, via this work queue of functions to be processed.
     val workQueue: Deque<GlobalDecl.Function> = LinkedList()
 
+    /**
+     * Processes [node] in the context that it may have been called from the shader stages in
+     * [callerShaderStages].
+     */
     fun traversalAction(
         node: AstNode,
         callerShaderStages: Set<ShaderStage>,
     ) {
+        // Recursively process children of this node.
         traverse(::traversalAction, node, callerShaderStages)
+        // If the node is a function call (which can occur in either statement or expression form)
+        // then the called function must be analysed.
         when (node) {
             is Statement.FunctionCall -> node.callee
             is Expression.FunctionCall -> node.callee
@@ -39,17 +59,21 @@ fun runFunctionToShaderStageAnalysis(
             if (scopeEntry !is ScopeEntry.Function) {
                 return
             }
+            // Either add a new result entry for this function, or add the relevant shader stages to
+            // an existing result entry for the function.
             if (calleeName in result) {
                 result[calleeName]!!.addAll(callerShaderStages)
             } else {
                 result[calleeName] = callerShaderStages.toMutableSet()
             }
+            // Add the called function to the work queue if not already present.
             if (scopeEntry.astNode !in workQueue) {
                 workQueue.addLast(scopeEntry.astNode)
             }
         }
     }
 
+    // Initialize the work queue with all shader stage entry point functions.
     for (function in tu.globalDecls.filterIsInstance<GlobalDecl.Function>()) {
         for (attribute in function.attributes) {
             val stages = mutableSetOf<ShaderStage>()
@@ -69,6 +93,7 @@ fun runFunctionToShaderStageAnalysis(
         }
     }
 
+    // Perform the BFS.
     while (workQueue.isNotEmpty()) {
         val function = workQueue.removeFirst()
         assert(function.name in result)
