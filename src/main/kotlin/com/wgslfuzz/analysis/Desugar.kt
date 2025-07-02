bcc2008b1511884339c0219a8ad51360448cb8a5
James Lee-Jones <james@lee-jones.co.uk>
2025-07-02 16:25:04 +0100

    headers + formatting

--- src/main/kotlin/com/wgslfuzz/analysis/Desugar.kt
+++ src/main/kotlin/com/wgslfuzz/analysis/Desugar.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 The wgsl-fuzz Project Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.wgslfuzz.analysis
 
 import com.wgslfuzz.core.AstNode
@@ -59,12 +75,13 @@ fun <T : AstNode> T.desugar(): T =
             // Otherwise, do nothing.
             is Statement.Loop ->
                 if (node.continuingStatement == null) {
-                    Statement.Loop(
-                        node.attributesAtStart,
-                        node.attributesBeforeBody,
-                        node.body,
-                        ContinuingStatement(statements = Statement.Compound(listOf())),
-                    ).desugar()
+                    Statement
+                        .Loop(
+                            node.attributesAtStart,
+                            node.attributesBeforeBody,
+                            node.body,
+                            ContinuingStatement(statements = Statement.Compound(listOf())),
+                        ).desugar()
                 } else {
                     null
                 }
@@ -73,12 +90,13 @@ fun <T : AstNode> T.desugar(): T =
             // Otherwise, do nothing.
             is Statement.If ->
                 if (node.elseBranch == null) {
-                    Statement.If(
-                        node.attributes,
-                        node.condition,
-                        node.thenBranch,
-                        Statement.Compound(listOf()),
-                    ).desugar()
+                    Statement
+                        .If(
+                            node.attributes,
+                            node.condition,
+                            node.thenBranch,
+                            Statement.Compound(listOf()),
+                        ).desugar()
                 } else {
                     null
                 }
