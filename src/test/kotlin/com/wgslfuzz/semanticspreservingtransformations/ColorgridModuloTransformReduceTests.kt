8c4ad588236cabd9d477b308b79367879c0477ec
Max Smith <103558620+Max-Sepp@users.noreply.github.com>
2025-07-16 13:54:08 +0100

    Disable Failing Tests for Colorgrid Modulo

--- src/test/kotlin/com/wgslfuzz/semanticspreservingtransformations/ColorgridModuloTransformReduceTests.kt
+++ src/test/kotlin/com/wgslfuzz/semanticspreservingtransformations/ColorgridModuloTransformReduceTests.kt
@@ -1,5 +1,6 @@
 package com.wgslfuzz.semanticspreservingtransformations
 
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import kotlin.test.assertNotEquals
 
@@ -11,5 +12,35 @@ class ColorgridModuloTransformReduceTests : TransformReduceTests() {
     fun `Check filenameNoExtension is not empty`() {
         // A solution for cases where IDEs are unable to detect child classes of test classes as valid test classes.
         assertNotEquals(filenameNoExtension, "")
+    }
+
+    @Disabled("TODO(https://github.com/mc-imperial/wgsl-fuzz/issues/137)")
+    @Test
+    override fun testAddDeadReturns() {
+        super.testAddDeadReturns()
+    }
+
+    @Disabled("TODO(https://github.com/mc-imperial/wgsl-fuzz/issues/138)")
+    @Test
+    override fun testAddDeadBreaks() {
+        super.testAddDeadBreaks()
+    }
+
+    @Disabled("TODO(https://github.com/mc-imperial/wgsl-fuzz/issues/139)")
+    @Test
+    override fun testAddDeadContinues() {
+        super.testAddDeadContinues()
+    }
+
+    @Disabled("TODO(https://github.com/mc-imperial/wgsl-fuzz/issues/140)")
+    @Test
+    override fun testAddIdentityOperations() {
+        super.testAddIdentityOperations()
+    }
+
+    @Disabled("TODO(https://github.com/mc-imperial/wgsl-fuzz/issues/141)")
+    @Test
+    override fun testMultipleTransformations() {
+        super.testMultipleTransformations()
     }
 }
