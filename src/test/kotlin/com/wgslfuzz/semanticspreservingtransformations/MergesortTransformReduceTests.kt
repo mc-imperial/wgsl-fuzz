cab7d5958e42c6d1515db66123f734b4ac32f169
Max Smith <103558620+Max-Sepp@users.noreply.github.com>
2025-07-17 18:48:08 +0100

    Remove disables from passing tests
    Tests now passing for:
    - Counting Sort
    - Floyd with Spaghetti
    - Merge Sort
    - Quicksort
    
    Fixes: #143
    Fixes: #144
    Fixes: #148
    Fixes: #151

--- src/test/kotlin/com/wgslfuzz/semanticspreservingtransformations/MergesortTransformReduceTests.kt
+++ src/test/kotlin/com/wgslfuzz/semanticspreservingtransformations/MergesortTransformReduceTests.kt
@@ -16,11 +16,9 @@
 
 package com.wgslfuzz.semanticspreservingtransformations
 
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import kotlin.test.assertNotEquals
 
-@Disabled("TODO(https://github.com/mc-imperial/wgsl-fuzz/issues/148)")
 class MergesortTransformReduceTests : TransformReduceTests() {
     override val filenameNoExtension: String
         get() = "mergesort"
