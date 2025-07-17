0e050ec9cf99f938050a5207c90650b5567f3ba8
Max Smith <103558620+Max-Sepp@users.noreply.github.com>
2025-07-17 16:26:44 +0100

    Collatz tests passing
    Reduced maxDepth to prevent using up to many resources and jUnit
    wrongly ignoring it.
    
    Fixes: #132
    Fixes: #133
    Fixes: #134
    Fixes: #135
    Fixes: #136

--- src/test/kotlin/com/wgslfuzz/semanticspreservingtransformations/CollatzTransformReduceTests.kt
+++ src/test/kotlin/com/wgslfuzz/semanticspreservingtransformations/CollatzTransformReduceTests.kt
@@ -16,7 +16,6 @@
 
 package com.wgslfuzz.semanticspreservingtransformations
 
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import kotlin.test.assertNotEquals
 
@@ -30,31 +29,26 @@ class CollatzTransformReduceTests : TransformReduceTests() {
         assertNotEquals(filenameNoExtension, "")
     }
 
-    @Disabled("TODO(https://github.com/mc-imperial/wgsl-fuzz/issues/132)")
     @Test
     override fun testAddDeadReturns() {
         super.testAddDeadReturns()
     }
 
-    @Disabled("TODO(https://github.com/mc-imperial/wgsl-fuzz/issues/133)")
     @Test
     override fun testAddDeadBreaks() {
         super.testAddDeadBreaks()
     }
 
-    @Disabled("TODO(https://github.com/mc-imperial/wgsl-fuzz/issues/134)")
     @Test
     override fun testAddDeadContinues() {
         super.testAddDeadContinues()
     }
 
-    @Disabled("TODO(https://github.com/mc-imperial/wgsl-fuzz/issues/135)")
     @Test
     override fun testAddIdentityOperations() {
         super.testAddIdentityOperations()
     }
 
-    @Disabled("TODO(https://github.com/mc-imperial/wgsl-fuzz/issues/136)")
     @Test
     override fun testMultipleTransformations() {
         super.testMultipleTransformations()
